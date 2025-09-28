d2905cbb45f1067d3779aa612d5d9a6af407a746
Kerntarn <fluk032548@gmail.com>
2025-09-28 20:38:25 +0000

    Add: campuses section extraction, location of university

--- utils/u_detail.py
+++ utils/u_detail.py
@@ -4,13 +4,13 @@ from .http import fetch
 BOLD_IN_BRACKET = re.compile(r"\(.*?<b>([A-Z ]+)</b>.*?\)")
 BRACKET = re.compile(r"[> ]\(([A-Z][A-Za-z]+)\)[< ]")
 UPPER_COMMA = re.compile(r"\(([A-Z]+),")
-RIGHTSIDE_TABLE = re.compile(r"<table class=\"infobox vcard\">.*?</table>", re.S)
-IN_PTAG = re.compile(r"<p[^>]*>.*?\b([A-Z]+)\b.*?</p>", re.S)
+RIGHTSIDE_TABLE = re.compile(r"(?s)<table class=\"infobox vcard\">.*?</table>")
+IN_PTAG = re.compile(r"(?s)<p[^>]*>.*?\b([A-Z]+)\b.*?</p>")
 ATAG = re.compile(r'<a [^>]*>.*?</a>')
 
 TH_TD = re.compile(r"(?is)<th\b[^>]*>\s*Established\b[^<]*</th>\s*<td\b[^>]*>(.*?)</td>")
 YRS = re.compile(r"([12]\d{3})")
-ESTAB = re.compile(r"established[^\d]{0,50}?([12]\d{3})", re.I)
+ESTAB = re.compile(r"(?i)established[^\d]{0,50}?([12]\d{3})")
 
 FACULTY_KEYWORDS = re.compile(
     r"(?i)\b("
@@ -23,19 +23,16 @@ DL = re.compile(r"<dl><dt>(.*?)</dt></dl>")
 DEL_TAG = re.compile(r"<[^>]+?>.*<\/[^>]+>")
 H3 = re.compile(r"<h3[^>]*>(.*?)<\/h3>")
 LI = re.compile(r"<li>(.*?)<\/li>")
-TAG_CONTENT= re.compile(r"<[^>]+?>(.*)<\/[^>]+?>")
+TAG_CONTENT= re.compile(r"<(\w+)[^>]*>(.*?)<\/\1>")
 
 INFOCARD_WEBSITE = re.compile(r'(?is)<table class="infobox vcard">.*?Website.*?href="(.*?)".*<\/table>')
 REFERENCES_KEYWORDS = re.compile(r"(?i)\b(References|External links)\b")
 HREF_HTTP = re.compile(r'(?i)<a[^>]*\bhref\s*=\s*(["\'])(https?://.*?)\1')
 BLACKLISTS = re.compile(r"(?i)(?:wiki|article|news|category|trending|[0-9]{2,}|travel|google|pedia|\.com)")
-INSTITUTION_KEYWORDS = re.compile(
-    r"(?i)\b("
-    r"University|College|Institute|Polytechnic|Academy|Faculty|School"
-    r"|Universidad|Instituto|Escuela|Politécnico|Tecnológico|Tecnológica"
-    r")\b"
-)
 OVERVIEW = re.compile(r"(?i)\b(Overview|About|History|Introduction|Background|General information)\b")
+
+CAMPUS_KEYWORDS = re.compile(r"(?i)\b(Campuses|Campus|Branches|Locations|Location|Main campus|Other campuses)")
+INFOCARD_LOCATION = re.compile(r'(?is)<table class="infobox vcard">.*?locality">(.*?)<\/div>.*?country-name">(.*?)<\/div>.*<\/table>')
     
 def _is_valid_website(m: str) -> bool:
     after = m.split('://', 1)[1]
@@ -78,7 +75,7 @@ def _extract_abbreviate(html, path):
         at = 3
         in_bracket = IN_PTAG.findall(html_first_section)
 
-    print(f'Abbreviate :          {in_bracket} {at}')
+    # print(f'Abbreviate :          {in_bracket} {at}')
 
     if len(in_bracket) == 0:
         return f"{make_abbreviation(path)} *"
@@ -97,7 +94,7 @@ def _extract_established_year(html):
         estab_data = estab_data.group(1) if estab_data else None
     if not estab_data:
         estab_data = 'N/A'
-    print(f'Established Year :   {estab_data} {at}')
+    # print(f'Established Year :   {estab_data} {at}')
     return estab_data
 
 def _clean(text):
@@ -152,22 +149,45 @@ def _extract_faculties(html):
     return result
 
 def _extract_campuses(html):
-    # campuses = set()
-    # for m in HREF_HTTP.finditer(html):
-    #     url = m.group(2)
-    #     text = m.group(0)
-    #     if _is_valid_website(url) and INSTITUTION_KEYWORDS.search(text):
-    #         campuses.add(url)
-    # # print(f'Campuses :           {campuses}')
-    # return list(campuses)
-    pass
+    html_sections = html.split('<div class=\"mw-heading mw-heading2\">')
+    result = []
+    for section in html_sections:
+        header2 = H2.search(section)
+        if header2 and CAMPUS_KEYWORDS.search(header2.group(1)):
+            result.append(section)
+    print(f'Campus Sections : {len(result)}')
+
+    return ''
+
+def _extract_location(html):
+    location = None
+    result = INFOCARD_LOCATION.search(html)
+    if result:
+        location = result.group(1)
+        country = result.group(2)
+        i = 0
+        if '>' in location:
+            s = TAG_CONTENT.findall(location)
+            while '>' in s[i][1]:
+                i += 1
+            location = s[i][1].strip()
+        i = 0
+        if '>' in country:
+            s = TAG_CONTENT.findall(country)
+            while '>' in s[i][1]:
+                i += 1
+            country = s[i][1].strip()            
+
+        print(f'Location :          {location} {country}')
+        return f'{location}, {country}'
+    return 'N/A'
 
 def _extract_website(html):
     website = None
     result = INFOCARD_WEBSITE.search(html)
     if result:
         website = result.group(1) 
-        print(f'Websites :          {website} (infocard)')
+        # print(f'Websites :          {website} (infocard)')
         return website  
 
     html_sections = html.split('<div class=\"mw-heading mw-heading2\">')
@@ -176,13 +196,13 @@ def _extract_website(html):
         header2 = H2.search(section)
         if header2 and REFERENCES_KEYWORDS.search(header2.group(1)):
             ref_sections.append(section)
-    print(f'Reference Sections : {len(ref_sections)}')
+    # print(f'Reference Sections : {len(ref_sections)}')
 
     for section in ref_sections[::-1]:
         websites = [m.group(2) for m in HREF_HTTP.finditer(section) if _is_valid_website(m.group(2))]
         break
-    print(f'Websites :          {websites}')
-    print('--------------------------------------')
+    # print(f'Websites :          {websites}')
+    # print('--------------------------------------')
     website = websites[0] if websites else []
     return website
 
@@ -201,5 +221,6 @@ def extract_universities_detail_from_university_page(path: str) -> dict: ##Abbr 
     campuses = _extract_campuses(body_part_html)
     faculties = _extract_faculties(body_part_html)
     website = _extract_website(body_part_html)
+    location = _extract_location(body_part_html)
 
-    return {'abbr': abbreviation, 'estab': estab_data, 'campuses': [], 'website': website, 'faculties': faculties}
+    return {'abbr': abbreviation, 'estab': estab_data, 'location': location, 'campuses': campuses, 'website': website, 'faculties': faculties}
